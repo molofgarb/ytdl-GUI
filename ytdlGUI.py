8071e7458f0d0829ebc5a429dd5b4c3cc9f2a4e5
molofgarb <50849715+molofgarb@users.noreply.github.com>
2022-04-16 11:37:04 -0400

    visual changes updated banner and README link to banner added logo/icon to tkinter window added info window to display information about program added labels to text fields fixed ytdl-GUI not working with non-youtube videos

--- ytdlGUI.py
+++ ytdlGUI.py
@@ -3,9 +3,15 @@
 
 import tkinter as tk
 from tkinter import filedialog
+
+import os
 import sys 
 import subprocess 
 import platform 
+import time
+
+import webbrowser
+
 from yt_dlp import YoutubeDL
 
 
@@ -36,36 +42,80 @@ def updateLabel(root, label, text):
     label.configure(text=text)
     root.update()
 
+class InfoWindow(tk.Toplevel):
+    def __init__(self, root):
+        super().__init__(root)
+
+        self.title("Info")
+        root.eval(f'tk::PlaceWindow {str(self)} center')
+
+        self.frame = tk.Frame(self)
+        self.frame.grid(row=0, padx=20, pady=20)
+
+        self.readmeButton = tk.Button(
+            self.frame, text = "Open Readme",
+            command = self.openReadme
+        )
+        self.readmeButton.grid(row=0, sticky="W", padx=5, pady=5)
+
+        self.repoLabel = tk.Label(
+            self.frame, text = "\nGitHub:",
+        )
+        self.repoLabel.grid(row=1, sticky="W")
+
+        self.repoLink = tk.Label(
+            self.frame, text = "https://github.com/molofgarb/ytdl-GUI",
+            fg = "blue", cursor = "hand2"
+        )
+        self.repoLink.grid(row=2)
+        self.repoLink.bind(
+            "<Button-1>", lambda e: webbrowser.open_new_tab("https://github.com/molofgarb/ytdl-GUI")
+        )
+
+        self.thanksLabel = tk.Label(
+            self.frame, text = "\nThank you for using ytdl-GUI!"
+        )
+        self.thanksLabel.grid(row=3)
+
+    def openReadme(self):
+        try:
+            os.startfile("README.md")
+        except:
+            raise Exception("Readme not found")
+
+
+
 class DownloadPrompt(tk.Toplevel):
-    def __init__(self, master, URLs = []):
-        super().__init__(master)
+    def __init__(self, root, URLs = []):
+        super().__init__(root)
 
         self.title("Download Confirmation")
-        master.eval(f'tk::PlaceWindow {str(self)} center')
+        root.eval(f'tk::PlaceWindow {str(self)} center')
+
+        self.frame = tk.Frame(self)
+        self.frame.grid(row=0, padx=2, pady=10)
 
         self.URLs = URLs
 
         # ------- WIDGETS -------
         self.questionLabel = tk.Label(
-            self,
-            text = "Do you want to download these youtube videos?"
+            self.frame, text = "Do you want to download these videos?"
         )
-        self.questionLabel.grid(column=0, row=0, sticky="N", columnspan=2)
-        self.columnconfigure(0, weight=1)
+        self.questionLabel.grid(sticky="N", columnspan=2)
         
         self.yesButton = tk.Button(
-            self,
-            text = "Yes",
+            self.frame, text = "Yes",
+            width=6,
             command = self.confirm
         )
-        self.yesButton.grid(column=0, row=1, sticky="N", padx=50, pady=20)
+        self.yesButton.grid(row=1, sticky="N", padx=30, pady=20)
 
         self.noButton = tk.Button(
-            self,
-            text = "No",
+            self.frame, text = "No",
+            width=6, 
             command = self.destroy
         )
-        self.noButton.grid(column=1, row=1, sticky="N", padx=50, pady=20)
+        self.noButton.grid(column=1, row=1, sticky="N", padx=30, pady=20)
 
     def confirm(self):
         self.destroy()
@@ -88,68 +138,72 @@ class MainWindow(tk.Tk):
 
         #initialize main frame (located within main window)
         self.frame = tk.Frame(self)
-        self.frame.grid(padx=20, pady=20)
+        self.frame.grid(padx=20, pady=5)
 
         # ------- WIDGETS -------
+        #label for directory
+        self.directoryLabel = tk.Label(
+            self.frame, text = "Output Path:"
+        )
+        self.directoryLabel.grid(sticky="W")
+
         #directory to download to
         self.directoryText = tk.Text(
-            self.frame,
-            height = 1,
-            width = 60,
+            self.frame, height = 1, width = 68,
         )
         self.directoryText.insert(tk.END, whereami)
-        self.directoryText.grid(column=0, row=0, padx=5, pady=5)
+        self.directoryText.grid(column=0, row=1, padx=5, pady=5)
 
         #button to open directory-choosing prompt
         self.directoryButton = tk.Button(
-            self.frame,
-            height = 0,
-            width = 0,
-            text = "...",
-            command = self.setDirectory
+            self.frame, height = 0, width = 0,
+            text = "...", command = self.setDirectory
         )
-        self.directoryButton.grid(column=1, row=0, padx=1, pady=1)
+        self.directoryButton.grid(column=1, row=1, padx=1, pady=1)
+
+        #input label
+        self.inputLabel = tk.Label(
+            self.frame, text = "Videos to download:"
+        )
+        self.inputLabel.grid(row=2, sticky="W")
         
         #input text box
         self.inputText = tk.Text(
-            self.frame,
-            height = 5,
-            width = 60
+            self.frame, height = 7, width = 68
         )
         self.inputText.insert(tk.END,
             "https://www.youtube.com/shorts/9p0pdiTOlzw\n" + #get wifi anywhere you go
             "https://www.youtube.com/watch?v=fFxySUC2vPc\n" + #python subprocess
             "https://youtu.be/Y_pbEOem2HU\n" + #vine boom
-            "https://youtu.be/jNQXAC9IVRw\n" #me at the zoo
+            "jNQXAC9IVRw\n" + #me at the zoo
+            "https://www.reddit.com/r/Eyebleach/comments/ml2y1g/dramatic_sable/"
         ) #default text
-        self.inputText.grid(column=0, row=1, padx=5, pady=5)
+        self.inputText.grid(row=3, padx=5, pady=5)
         
         #button to send text box input
         self.inputButton = tk.Button(
-            self.frame,
-            text = "Download", 
+            self.frame, text = "Download", 
             command = self.inputURLs
         )
-        self.inputButton.grid(column=0, row=2, padx=5, pady=5)
+        self.inputButton.grid(row=4, padx=5, pady=5)
         
         # Label!!
         self.statusLabel = tk.Label(
-            self.frame,
-            text = "I will be overwritten!"
+            self.frame, text = "Awaiting URL input"
         )
-        self.statusLabel.grid(column=0, row=3, padx=5, pady=5)
+        self.statusLabel.grid(row=5, padx=2, pady=2)
+
+        #info label
+        self.infoButton = tk.Button(
+            self.frame, text = "Info",
+            command = self.openInfoWindow
+        )
+        self.infoButton.grid(row=6, sticky="E", pady=8)
 
     #takes inputs from <inputtxt> and stores them in <URLs>
     def inputURLs(self):
         input1 = self.inputText.get(1.0, tk.END)
         URLs = input1.split() 
-        for i in range(len(URLs)):
-            thisURL = URLs[i].split("/")
-            if (thisURL[2] != "youtu.be" and thisURL[2] != "www.youtube.com"):
-                raise Exception("Incorrect URL Type")
-            else:
-                if (thisURL[3] == "shorts"):
-                    URLs[i] = thisURL[4]
         updateLabel(self, self.statusLabel, "URLs Received!")
         DownloadPrompt(self, URLs)
         
@@ -159,7 +213,11 @@ class MainWindow(tk.Tk):
         ydl = YoutubeDL(options)
         for i in range(len(URLs)):
             updateLabel(self, self.statusLabel, "Downloading video " + str(i + 1) + "...")
-            ydl.download(URLs[i])
+            try:
+                ydl.download(URLs[i])
+            except:
+                updateLabel(self, self.statusLabel, f'Error: {URLs[i]} is not a valid URL')
+                time.sleep(2)
         updateLabel(self, self.statusLabel, "Done downloading!")
 
     #uses tkinter's askdirectory dialog to set directory in text box
@@ -172,11 +230,14 @@ class MainWindow(tk.Tk):
     def saveDirectory(self):
         self.outDir = self.directoryText.get(1.0, tk.END)
 
-    
+    def openInfoWindow(self):
+        InfoWindow(self)
+
 
 
 def main():
     root = MainWindow()
+    root.iconbitmap("resources/logo.ico")
     root.mainloop()
 
 
